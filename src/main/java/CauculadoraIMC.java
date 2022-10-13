a7cfea650938c2ad28b6745c61d4efd0c6fe6d4c
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-10-13 02:27:38 -0300

    commit arrays

--- src/main/java/CauculadoraIMC.java
+++ src/main/java/CauculadoraIMC.java
@@ -27,6 +27,58 @@ public class CauculadoraIMC {
         System.out.println("Este é o seu IMC = " + imc);
         System.out.printf("Este é o seu IMC = %.2f\n", imc);
         System.out.printf("Olá,  %s Idade %d anos!\n", nome, idade);
+
+        // https://www.calculoimc.com.br/tabela-de-imc/
+
+        if(imc < 17){
+            System.out.println("Muito abaixo do peso");
+        } else if(imc > 17 && imc <= 18.49){
+            System.out.println("Abaixo do peso");
+        }else if(imc >= 18.5 && imc <= 24.99){
+            System.out.println("Peso Normal");
+        }else if(imc >= 25 && imc <= 29.99){
+            System.out.println("Acima do peso");
+        }else if(imc >= 30 && imc <= 34.99){
+            System.out.println("Obesidade I");
+        }else if(imc >= 35 && imc <= 39.99){
+            System.out.println("Obesidade II (Severa)");
+        }else {
+            System.out.println("Obesidade III(Mórbida");
+        }
+
+        // switch-case
+        System.out.println("Selecione uma dieta ( 1 a 3 )");
+        System.out.println("1 - Agua");
+        System.out.println("2 - Fruta");
+        System.out.println("2 - Batata");
+        int dieta = entrada.nextInt();
+
+        switch (dieta) {
+            case 1:
+                System.out.println("Você escolheu a dieta da Água!");
+                break; // impedir de testar a condição abaixo
+            case 2:
+                System.out.println("Você escolheu a dieta da fruta!");
+                break;
+            case 3:
+                System.out.println("Você escolheu a dieta da Batata!");
+                break;
+            default: // **else**
+                System.out.println("Essa dieta não existe!");
+        }
+
+        System.out.println("Você tem quantos alimentos preferidos?");
+        int totalAlimentos = entrada.nextInt();
+
+        for (int i = 0; i < totalAlimentos; i++) {
+            System.out.println(i);
+        }
+        int i = 0;
+       /* while(i < totalAlimentos) {
+            System.out.println(i);
+            i++;
+        }
+        System.out.println(i);*/
     }
 
 }
---
+++ src/main/java/EstudoArrays.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class EstudoArrays {
+    public static  void main (String[] args){
+        int[] numeros = {1, 2,3,4,5};
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println("A poisção " + i + " com valor de " + numeros[i]);
+        }
+
+        int[] numeros2 = new int [50];
+        numeros2[0] = 500;
+        numeros2[1] = -200;
+        //...
+        numeros2[49] = 1;
+
+        double[] notas = new double[5];
+        String[] nomes = {"José Almir", "Renato Pereira", "Victor Icoma"};
+        String[] sobrenomes = new String[10];
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int quantidade = quantidade = entrada.nextInt();
+        boolean[] valores = new boolean[quantidade];
+
+        //Exemplo
+        System.out.println("Digite o total de notas: ");
+        int totalNotas = entrada.nextInt();
+        entrada.nextLine();
+
+        double[] notasProva = new double[totalNotas];
+
+        for (int i = 0; i < totalNotas; i++){
+            System.out.println("Digite o valor da nota " + (i+1) + "ºnota: ");
+            notasProva[i] = entrada.nextDouble();
+        }
+
+        double soma = 0.0;
+        for (double nota : notasProva){
+            soma += nota;
+        }
+        double media = soma / notasProva.length;
+
+        // Condição ternária
+        String mensagem = (media < 7) ? "Você está reprovado" : "Você está aprovado";
+        System.out.println(mensagem);
+
+        // Default values
+        int numero; //padrao  = 0
+        double nota2; // padrão = 0.0
+        long populacao; // padrão = 0
+        boolean teste; // padrão = false
+        String nome; // Não primitivo padrão null (ausência de objeto na variável)
+
+        // Wrapper classes
+
+        Integer numero2 = 1;
+        Double nota3 = 1.0;
+        Long pop2 = 1L;
+        Boolean teste2 = false.
+    }
+}
